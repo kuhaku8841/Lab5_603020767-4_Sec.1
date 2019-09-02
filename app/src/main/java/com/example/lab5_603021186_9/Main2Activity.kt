5b9025e286852d23cbc247a4d839a223e0461455
psaharat <psaharat@kkumail.com>
2019-09-02 17:23:50 +0700

    66

---
+++ app/src/main/java/com/example/lab5_603021186_9/Main2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.lab5_603021186_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
